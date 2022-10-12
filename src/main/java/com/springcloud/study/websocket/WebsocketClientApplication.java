cdd57d4e0bf62f08b83924452a8db601ea169e9c
tanzhiwei <tanzhiwei0526@163.com>
2022-10-12 11:10:31 +0800

    版本初始化

--- src/main/java/com/springcloud/study/websocket/WebsocketClientApplication.java
+++ src/main/java/com/springcloud/study/websocket/WebsocketClientApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 类 描 述: 启动类
+ * 类 描 述: WebSocket启动类
  * 作   者: 谭志伟
  * 时   间: 2022/10/11  18:42
  */
@@ -15,7 +15,12 @@ public class WebsocketClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketClientApplication.class, args);
-        log.info("启动成功。。。。。。。。。。。。。。。");
+        log.info("(♥◠‿◠)ﾉﾞ  WebSocket启动成功   ლ(´ڡ`ლ)" +
+                " _                 \n" +
+                "| |_ ______      __\n" +
+                "| __|_  /\\ \\ /\\ / /\n" +
+                "| |_ / /  \\ V  V / \n" +
+                " \\__/___|  \\_/\\_/ ");
     }
 
 }
