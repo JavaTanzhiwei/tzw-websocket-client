22f6ece991213e34424ed6c41f866f4962790e51
tanzhiwei <tanzhiwei0526@163.com>
2022-10-12 11:22:44 +0800

    版本初始化

--- src/main/java/com/springcloud/study/websocket/clients/ClientNotice.java
+++ src/main/java/com/springcloud/study/websocket/clients/ClientNotice.java
@@ -6,14 +6,14 @@ import lombok.SneakyThrows;
 import java.net.URI;
 
 /**
- * 类 描 述: 下线通知
+ * 类 描 述: WebSocket通知
  * 作   者: 谭志伟
  * 时   间: 2022/10/11  18:42
  */
-public class ClientOffNotice {
+public class ClientNotice {
     @SneakyThrows
     public static void main(String[] args) {
-        MyWebSocketClient client = new MyWebSocketClient( new URI("ws://localhost:8012/websocket/LOGOFF_NOTICE/36pE6sLIQJLSdgdYXtjRl2LM8w69FGCq"));
+        MyWebSocketClient client = new MyWebSocketClient( new URI("ws://localhost:8012/websocket/KAFKA_NOTICE/36pE6sLIQJLSdgdYXtjRl2LM8w69FGCq"));
         Clients.connect(client);
     }
 }
