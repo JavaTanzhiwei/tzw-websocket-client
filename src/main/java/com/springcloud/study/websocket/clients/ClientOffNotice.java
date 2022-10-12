733f91ffed15d2335c89950c0eb26d8368f16775
tanzhiwei <tanzhiwei0526@163.com>
2022-10-12 09:20:23 +0800

    版本初始化

--- src/main/java/com/springcloud/study/websocket/clients/ClientOffNotice.java
+++ src/main/java/com/springcloud/study/websocket/clients/ClientOffNotice.java
@@ -10,10 +10,10 @@ import java.net.URI;
  * 作   者: 谭志伟
  * 时   间: 2022/10/11  18:42
  */
-public class Client_1 {
+public class ClientOffNotice {
     @SneakyThrows
     public static void main(String[] args) {
         MyWebSocketClient client = new MyWebSocketClient( new URI("ws://localhost:8012/websocket/LOGOFF_NOTICE/USER202203250000001:f63c418f-e702-4658-b9b7-c45e99bc7c26"));
-        Client_4.connect(client);
+        Clients.connect(client);
     }
 }
--- src/main/java/com/springcloud/study/websocket/clients/Clients.java
+++ src/main/java/com/springcloud/study/websocket/clients/Clients.java
@@ -2,6 +2,7 @@ package com.springcloud.study.websocket.clients;
 
 import com.springcloud.study.websocket.bean.MyWebSocketClient;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
 
@@ -10,25 +11,26 @@ import java.net.URI;
  * 作   者: 谭志伟
  * 时   间: 2022/10/11  18:42
  */
-public class Client_4 {
+@Slf4j
+public class Clients {
     static Boolean isDo = true;
 
     @SneakyThrows
     public static void main(String[] args) {
-        MyWebSocketClient client = new MyWebSocketClient( new URI("ws://localhost:8012/websocket/LOGOFF_NOTICE/4:64378994-d822-49ce-8242-b9a4c237405c"));
+        MyWebSocketClient client = new MyWebSocketClient(new URI("ws://localhost:8012/websocket/LOGOFF_NOTICE/4:64378994-d822-49ce-8242-b9a4c237405c"));
         connect(client);
     }
 
-    static void connect(MyWebSocketClient client) throws InterruptedException {
+    public static void connect(MyWebSocketClient client) throws InterruptedException {
         client.connect();
         Thread.sleep(2_000);
-        while (isDo){
-            if(!client.isOpen()){
+        while (isDo) {
+            if (!client.isOpen()) {
                 try {
                     client.reconnect();
                 } catch (Exception e) {
                     e.printStackTrace();
-                    System.out.println("====================================================================================");
+                    log.info("WebSocket连接异常");
                 }
             }
         }
